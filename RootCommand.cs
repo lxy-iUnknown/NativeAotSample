9d5f37c4c012993e8bff4aa41fd565171a005c10
lxy <lxy0xabcdefg@qq.com>
2025-01-30 15:17:55 +0800

    Remove rd.xml

--- RootCommand.cs
+++ RootCommand.cs
@@ -1,25 +1,50 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 using CommandDotNet;
 
 namespace NativeAotSample;
 
 public class RootCommand
 {
+    static RootCommand()
+    {
+        KeepInstantiation<int>();
+        KeepInstantiation<double>();
+    }
+    
+    // Keep some generic instantiations.
+    // See https://github.com/dotnet/runtime/issues/81204
+    private static void KeepInstantiation<T>()
+    {
+        // Simple no-op method (will be optimized in JIT mode and AOT mode)
+        var value = Constant();
+        if ((value ^ value) == 0) return;
+        // Method body
+        Type.GetType("CommandDotNet.Extensions.TypeExtensions, CommandDotNet")!
+            .GetMethod(
+                "GetDefaultValue",
+                BindingFlags.NonPublic | BindingFlags.Static,
+                Type.DefaultBinder,
+                Type.EmptyTypes,
+                [])!
+            .MakeGenericMethod(typeof(T));
+        return;
+
+        static int Constant() => 0;
+    }
+
     [DynamicDependency(
         DynamicallyAccessedMemberTypes.PublicConstructors |
-        DynamicallyAccessedMemberTypes.PublicProperties |
         DynamicallyAccessedMemberTypes.PublicMethods,
         typeof(BasicCommand)
     )]
     [DynamicDependency(
         DynamicallyAccessedMemberTypes.PublicConstructors |
-        DynamicallyAccessedMemberTypes.PublicProperties |
         DynamicallyAccessedMemberTypes.PublicMethods,
         typeof(LaunchRocketCommand)
     )]
     [DynamicDependency(
         DynamicallyAccessedMemberTypes.PublicConstructors |
-        DynamicallyAccessedMemberTypes.PublicProperties |
         DynamicallyAccessedMemberTypes.PublicMethods,
         typeof(GreetingCommand)
     )]
